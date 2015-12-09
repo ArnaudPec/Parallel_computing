d9cd3958929b4907152e436b66c581bb6bf1a941
Arnaud Pecoraro <arnaud.pecoraro@utt.fr>
2015-12-09 19:36:26 +0100

    working on small set

--- Gpu_Bitonic_Sort/bitonic_kernel.cu
+++ Gpu_Bitonic_Sort/bitonic_kernel.cu
@@ -58,7 +58,7 @@ __global__ void bitonic_one_block(int *data, int N)
 
 __global__ void bitonic(int *data, int j, int k)
 {
-    i = threadIdx.x + blockDim.x*blockIdx.x;
+    int i = threadIdx.x + blockDim.x*blockIdx.x;
     int ixj=i^j; // Calculate indexing!
     if ((ixj)>i)
     {
@@ -73,9 +73,9 @@ void bitonic_gpu(int *data, int N){
     const int size = N*sizeof(int);
 
     cudaMalloc((void**)&d_data, size);
-
-    dim3 dimBlock(N);
-    dim3 dimGrid(N/512);
+    int mininal_size = min(N,512); //bc the size should not be more than 512
+    dim3 dimBlock(mininal_size);
+    dim3 dimGrid(N/512+1);// number of grid + remaining chunck
 
     cudaMemcpy(d_data, data, size,cudaMemcpyHostToDevice);
 
